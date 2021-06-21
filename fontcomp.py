9b20f2ef6c9c674aa380703d4d6bb82f108d10e8
ha4 <C:\MAIL>
2021-06-22 00:31:52 +0500

    fontcopress flat freq

--- fontcomp.py
+++ fontcomp.py
@@ -23,39 +23,6 @@ def chr_data(a):
         bs.append(b[chr_i0(a,i)])
     return bs
 
-def chr_nibs(a):
-    global b
-    bs=[]
-    for i in range(8):
-        j=b[chr_i0(a,i)]
-        bs.append(j>>4)
-        bs.append(j&15)
-    return bs
-
-def chr_bits(a):
-    global b
-    bs=""
-    for i in range(8):
-        bs+="{:08b}".format(b[chr_i0(a,i)])
-    return bs
-
-def chr_rle(bs,n=0): #
-    global b
-    ps=""
-    qs=""
-    ss=[]
-    for s in bs:
-        if s!=ps:
-            if(qs!=""): ss.append(qs)
-            qs=s
-            ps=s
-        else:
-            qs+=s
-            if len(qs)==n:
-                ss.append(qs)
-                qs=""
-                ps=""
-    return ss
 
 def freqs(lst):
     f={}
@@ -64,36 +31,16 @@ def freqs(lst):
         except: f[i]=1
     return f
 
-def freqnib(lst):
-    f={}
-    for i in lst:
-        n=i>>4;
-        try: f[n]+=1
-        except: f[n]=1
-        n=i&15;
-        try: f[n]+=1
-        except: f[n]=1
-    return f
-
-def freqsb(lst):
-    f={}
-    f['0']=0
-    f['1']=0
-    for i in lst:
-        for b in "{:08b}".format(i):
-            f[b]+=1
-    return f
-
-def all_rle():
-    m=[]
-    for a in range(96):
-        m+=chr_rle(chr_bits(chr(a+32)),8)
-    return m
-
 def freq_scale(a,f):
     fo={}
     for k,fv in a.items():
         fo[k]=int(fv/f)
+    return fo
+
+def freq_flat(a,f):
+    fo={}
+    for k,fv in a.items():
+        fo[k]=1
     return fo
 
 def symap(f): return sorted(f, key=f.get, reverse=True)
@@ -119,8 +66,7 @@ def prindict(m,f,full=False):
 def prbin(bj): return ("{:08b}".format(bj)).replace("0",".")
 
 def prindata(bs):
-    for e in bs:
-        print(prbin(e))
+    for e in bs: print(prbin(e))
 
 def princhr(a):
     global c
@@ -140,112 +86,6 @@ def prinstr(s):
             i0=chr_i0(c)+i*32
             print("{} ".format(prbin(b[i0])),end='')
         print()
-
-# 24 values
-def huff45(i):
-    if i<8: return "{:04b}".format(i+8)
-    else:   return "{:05b}".format(i-8)
-def dehff45(s):
-    if s[0]=='1':return int(s[0:4],2)-8,s[4:]
-    else:        return int(s[0:5],2)+8,s[5:]
-
-# 36 values
-def huff36(i):
-    if i<4:      return "{:03b}".format(i+4)
-    else:        return "{:06b}".format(i)
-def dehff36(s):
-    if s[0]=='1':return int(s[0:3],2)-4,s[3:]
-    else:        return int(s[0:6],2),s[6:]
-
-# 20 values
-def huff35(i):
-    if i<4:      return "{:03b}".format(i+4)
-    else:        return "{:05b}".format(i-4)
-def dehff35(s):
-    if s[0]=='1':return int(s[0:3],2)-4,s[3:]
-    else:        return int(s[0:5],2)+4,s[5:]
-
-# 24 values
-def huff346(i):
-    if i<4:      return "{:03b}".format(i+4)
-    elif i<8:    return "{:04b}".format(i)
-    else:        return "{:06b}".format(i-8)
-def dehff346(s):
-    if s[0]=='1':return int(s[0:3],2)-4,s[3:]
-    elif s[1]=='1':return int(s[0:4],2),s[4:]
-    else:        return int(s[0:6],2)+8,s[6:]
-
-# 22 values
-def huff246(i):
-    if i<2:    return "{:02b}".format(i+2)
-    elif i<6:  return "{:04b}".format(i+2)
-    else:      return "{:06b}".format(i-6)
-def dehff246(s):
-    if s[0]=='1':return int(s[0:2],2)-2,s[2:]
-    elif s[1]=='1': return int(s[0:4],2)-2,s[4:]
-    else:        return int(s[0:6],2)+6,s[6:]
-
-# 28 values
-def huff3457(i):
-    if i<4:     return "{:03b}".format(i+4)
-    elif i<8:   return "{:04b}".format(i)
-    elif i<12:  return "{:05b}".format(i-4)
-    else:       return "{:07b}".format(i-12)
-def dehff3457(s):
-    if s[0]=='1':  return int(s[0:3],2)-4,s[3:]
-    elif s[1]=='1':return int(s[0:4],2),s[4:]
-    elif s[2]=='1':return int(s[0:5],2)+4,s[5:]
-    else:          return int(s[0:7],2)+12,s[7:]
-
-# 20 values
-def huff3456(i):
-    if i<4:     return "{:03b}".format(i+4)
-    elif i<8:   return "{:04b}".format(i)
-    elif i<12:  return "{:05b}".format(i-4)
-    else:       return "{:06b}".format(i-12)
-def dehff3456(s):
-    if s[0]=='1':  return int(s[0:3],2)-4,s[3:]
-    elif s[1]=='1':return int(s[0:4],2),s[4:]
-    elif s[2]=='1':return int(s[0:5],2)+4,s[5:]
-    else:          return int(s[0:6],2)+12,s[6:]
-
-# 29 values
-def huff1467(i):
-    if i==0:  return "1"
-    elif i<5: return "{:04b}".format(i+3)
-    elif i<13:return "{:06b}".format(i+3)
-    else:     return "{:07b}".format(i-13)
-def dehff1467(s):
-    if s[0]=='1':  return 0,s[1:]
-    elif s[1]=='1':return int(s[0:4],2)-3,s[4:]
-    elif s[2]=='1':return int(s[0:6],2)-3,s[6:]
-    else:          return int(s[0:7],2)+13,s[7:]
-
-# 23 values
-def huff1357(i):
-    if i==0:  return "1"
-    elif i<3: return "{:03b}".format(i+1)
-    elif i<7: return "{:05b}".format(i+1)
-    else:     return "{:07b}".format(i-7)
-def dehff1357(s):
-    if s[0]=='1':  return 0,s[1:]
-    elif s[1]=='1':return int(s[0:3],2)-1,s[3:]
-    elif s[2]=='1':return int(s[0:5],2)-1,s[5:]
-    else:          return int(s[0:7],2)+7,s[7:]
-
-def pakdata(m,bs):
-    p=''
-    for e in bs:
-        n=m.index(e)
-        p+=huff(n) #+' '
-    return p
-
-def unpdata(m,p):
-    bs=[]
-    for e in range(8):
-        v,p=dehff(p)
-        bs.append(m[v])
-    return bs
 
 def prinbytein(x):
     global b
@@ -317,7 +157,7 @@ def patchfnt():
     b[746]=(b[746]-64)<<1
     #N
     b[270]-=64
-    #b[302]+=64
+    #b[302]+=32
     #x
     b[600]-=64
     b[632]+=64
@@ -343,19 +183,23 @@ def patchfnt():
     b[84]+=64+16
     b[148]+=16
     b[180]+=16
-
+    #[
+ #   b[283]>>=1
+ #   b[315]>>=1
+ #   b[347]>>=1
+ #   b[379]>>=1
+ #   b[411]>>=1
+ #   b[443]>>=1
+    #]
+    b[285]<<=1
+    b[317]<<=1
+    b[349]<<=1
+    b[381]<<=1
+    b[413]<<=1
+    b[445]<<=1
     #test
     #b[166]=0
     #b[433]=0
-
-def testhuff(f,g):
-    tl=0
-    for i in range(24):
-        sq=f(i)
-        n2,s=g(sq)
-        tl+=len(sq)
-        print("{:2}".format(i),"{:2}".format(n2),sq)
-    print("totallen:",tl)
 
 def testall():
     global m
@@ -379,23 +223,10 @@ def testall():
 
 def ransenc_ss(code,cum_freq,symb_fq,fq_total): # 
     p=""
-    #xmax=32
-    #renormalize
-    #while code>=xmax:
-    #    p+="{:b}".format(code%2)
-    #    code//=2
     code=(code//symb_fq)*fq_total+cum_freq+(code%symb_fq)
     return code,p
 def ransdec_ss(code,cum_freq,symb_fq,fq_total,p):
     code=symb_fq*(code//fq_total)+(code%fq_total)-cum_freq
-    #xmax=32
-    #renormalize
-    #while code < xmax:
-    #    code*=2
-    #    if len(p)>0:
-    #        code+=int(p[-1],2)
-    #        p=p[:-1]
-    #    else: break
     return code,p
 
 RC_MAXVAL=0xFFFFFFFF
@@ -455,7 +286,6 @@ def rcenc_ss(low,rang,cum_freq,symb_fq,fq_total,p):
         rang<<=8
         low=(low<<8)&RC_MAXVAL
     return low,rang,p
-
     
 def rdecoder_ini(p):
     low=0
@@ -516,15 +346,11 @@ def ransenc(bs):
     global c
     x=0
     ftot=rs[-1]
-    po=""
-    print(x,':',po)
     for ss in bs:
         sn=m.index(ss)
         fs=c[ss]
         cf=rs[sn]
         x,p=ransenc_ss(x,cf,fs,ftot)
-        print(x,ss,sn,fs,cf,p)
-        po+=p
     return "{:b}".format(x)
 
 def rslook(rs,x):
@@ -540,10 +366,8 @@ def ransdec(pp,cnt):
     global rs
     global c
     x=int(pp,2)
-    #pp=pp[:-45]
     ftot=rs[-1]
     bs=[]
-    print(x,":",pp)
     for i in range(cnt):
         snr=x%ftot
         sn,cf=rslook(rs,snr)
@@ -551,72 +375,33 @@ def ransdec(pp,cnt):
         fs=c[ss]
         bs.insert(0,ss)
         x,pp=ransdec_ss(x,cf,fs,ftot,pp)
-        print(x,ss,pp)
     return bs
 
-def testrans():
+def stat(lens):
+    nsym=len(lens)
     tl=0
     tll=0
     ml=0
-    nl=100
+    nl=1e6
+    for l in lens:
+        tl+=l
+        tll+=l*l
+        ml=max(ml,l)
+        nl=min(nl,l)
+    al=tl/nsym
+    se=math.sqrt(tll/nsym-al*al)
+    print("total:",tl, "average",al, "stderr",se, "min",nl, "max",ml)
+
+def testrans():
+    lens=[]
     for i in range(96):
         s=chr(32+i)
         ss=chr_data(s)
         po=ransenc(ss)
         ls=len(po)
-        ml=max(ml,ls)
-        nl=min(nl,ls)
-        tl+=ls
-        tll+=ls*ls
+        lens.append(ls)
         print(s,ls,po)
-    al=tl/96
-    print(tl, al, math.sqrt(tll/96-al*al), nl, ml)
-
-
-def simplenc(bs):
-    global m
-    dic_sz=24
-    x=0
-    #print(x,':')
-    for ss in bs:
-        sn=m.index(ss)
-        x=x*dic_sz+sn
-        #print(x,ss)
-    return "{:b}".format(x)
-
-def simpldec(pp):
-    global m
-    dic_sz=24
-    x=int(pp[-45:],2)
-    pp=pp[:-45]
-    bs=[]
-    print(x,":",pp)
-    for i in range(8):
-        sn=x%dic_sz
-        ss=m[sn]
-        bs.insert(0,ss)
-        x=x//dic_sz
-        #print(x,ss)
-    return bs
-
-def testsimpl():
-    tl=0
-    tll=0
-    ml=0
-    nl=100
-    for i in range(96):
-        s=chr(32+i)
-        ss=chr_data(s)
-        po=simplenc(ss)
-        ls=len(po)
-        ml=max(ml,ls)
-        nl=min(nl,ls)
-        tl+=ls
-        tll+=ls*ls
-        print(s,ls,po)
-    al=tl/96
-    print(tl, al, math.sqrt(tll/96-al*al), nl, ml)
-
+    stat(lens)
 
 #
 #
@@ -624,24 +409,22 @@ def testsimpl():
 print(os.getcwd())
 
 b=readfont("Cbios_8x8.bin")
-patchfnt()
+#patchfnt()
 
-#c=freqs(b)
+c=freqs(b)
 #c=freq_scale(c,2.9)
-c=freqs(all_rle())
-#c=freqnib(b)
-#c=freqsb(b)
+c=freq_flat(c,0)
+c[0]=2
+c[1]=2
 m=symap(c)
 #m=symap2(c)
 rs,ftot=rangs(m,c)
 #print(c)
-#c['0']=15
-#c['1']=1
 #prindict(m,c)
-prindict(m,c,full=True)
+#prindict(m,c,full=True)
 #print(m)
 #prindata(m)
-print(rs,ftot)
+#print(rs,ftot)
 
 #v=b.copy()
 #v.sort()
@@ -657,48 +440,21 @@ print(rs,ftot)
 # dble <188(28,5) a673(513,5) r594(530,2) N(270,0)
 # trpl                                    x600(536,2)
 #
+# max entrop symbols: $:33 $:43 ):34 3:35 4:36 <:35 >:35
+#      @:40 B:41 E:35 F:34 J:33 K:41 L:33 N:36 Q:36 R:36 S:34 ]:35
+#      d:33 j:33 k:42 t:36 
 
+"""
+ss=chr_data('&')
+prindata(ss)
+o=ransenc(ss)
+print(o,'len',len(o))
+r=ransdec(o,8)
+prindata(r)
+"""
+testrans()
 
-#huff,dehff=huff45,dehff45     #3205bit avg33.39 stde1.3 32..37 24
-#huff,dehff=huff36,dehff36     #3072bit avg32.00 stde4.9 24..42 36
-#huff,dehff=huff35,dehff35     #2816bit avg29.33 stde3.3 24..36 20
-#huff,dehff=huff346,dehff346   #2826bit avg29.44 stde3.8 24..39 24
-huff,dehff=huff246,dehff246   #2668bit avg27.79 stde5.1 16..36 22
-#huff,dehff=huff3457,dehff3457 #2819bit avg29.36 stde4.2 24..42 28
-#huff,dehff=huff1467,dehff1467 #2717bit avg28.30 stde6.9  8..42 29
-#huff,dehff=huff1357,dehff1357 #2658bit avg27.69 stde7.3  8..42 23
-#huff,dehff=huff3456,dehff3456 #2756bit avg28.71 stde3.4 24..38 20
-
-
-#ss=chr_data('l')
-#ssb=chr_bits('k')
-#ssn=chr_nibs('&')
-#prindata(ss)
-#ps=pakdata(m,ss)
-#us=unpdata(m,ps)
-#print(ps,len(ps))
-#prindata(us)
-#o=ransenc(ss)
-#o=rcenc(ss)
-#rce_lr(ss)
-#o=ransenc(ssb)
-#o=ransenc(ssn)
-#o=simplenc(ss)
-#print(o,'len',len(o))
-#r=ransdec(o,8)
-#rn=ransdec(o,16)
-#rb=ransdec(o,64)
-#r=simpldec(o)
-#prindata(r)
-
-
-#testrans2()
-#testsimpl()
-#testrans()
-#testall()
-#testhuff(huff,dehff)
-
-#princhr("&")
+#princhr("}")
 
 #prinstr(" !\"#$%&\'")
 #prinstr("()*+,-./")
