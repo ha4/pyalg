6b241c5a6ea74b8ed77091b2d3e595777251abf9
ha4 <C:\MAIL>
2021-06-27 02:27:35 +0500

    fontcopress bit codiding WIP 2"

--- fontcb.py
+++ fontcb.py
@@ -37,60 +37,70 @@ def prstream(cd):
         print(bx,end='')
     print()
 
-RADIX=1968
+RADIX=256
 def rcomp(a):
     cd=chr_data(a)
-    ct=[1,1,0]
+    ct=[0,0]
     l=0
-    r=RADIX
-    ob=[]
-    def renorm(l,r):
-        ob.append(l%RADIX)
-        r*=RADIX
-        l%=RADIX
-        return l,r
+    r=RADIX-1
+    ob=""
+    def renorm(l,r,v):
+        #print("renormE")
+        while r<RADIX:
+            if l>RADIX:
+                if l>=2*RADIX:
+                    v+="1"
+                    l-=2*RADIX
+                else:
+                    l-=RADIX
+            else:
+                v+="0"
+            l*=2
+            r*=2
+        return l,r,v
     for bx in binstream(cd):
-        ft=ct[0]+ct[1]
-        spl=r*ct[0]//ft
+        ft=ct[0]+ct[1]+2
+        #spl=r*ct[0]//ft
+        r//=ft
         if bx:
-            l+=spl
-            r+=spl
+            l+=(ct[0]+1)*r
+            r*=ct[1]+1
         else:
-            r=spl
-        if r<RADIX:
-            l,r=renorm(l,r)
+            l+=0*r
+            r*=ct[0]+1
+        l,r,ob=renorm(l,r,ob)
         ct[bx]+=1
-        print(l,r,ct[0],ct[1])
-    ob.append(l%RADIX)
-    l//=RADIX
-    ob.append(l)
-    ob.append(0)
+        print(bx,l,r,ct[0],ct[1])
     return ob
 def rdeco(v):
     cd=[]
-    ct=[0,0,0]
+    ct=[1,1,0]
     l=0
-    r=1
+    r=RADIX-1
     def renorm(l,r,v):
-        print("renorm")
-        r*=RADIX
-        l*=RADIX
-        l+=v[0]
-        print(l,r)
-        return l,r,v[1:]
+        #print("renormD")
+        while r<RADIX:
+            r*=2
+            l*=2
+            l+=int(v[0],2)
+            v=v[1:]
+            print("renormD l,r",l,r,v)
+        return l,r,v
     for _ in range(64):
-        if r < RADIX:
-            l,r,v=renorm(l,r,v)
+        l,r,v=renorm(l,r,v)
         ft=ct[0]+ct[1]+2
         spl=r*(ct[0]+1)//ft
+        print("l,r,spl",l,r,spl)
+        r//=ft        
         bx=l>=spl and 1 or 0
         if bx:
-            l-=spl
-            r-=spl
+            l+=(ct[0]+1)*r
+            r*=ct[1]+1
         else:
-            r=spl
+            r*=ct[0]+1
+        ct[bx]+=1
         cd.append(bx)
-        print(len(cd),cd)
+        print(bx)
     return cd
 
 print(os.getcwd())
@@ -98,5 +108,5 @@ b=readfont("Cbios_8x8.bin")
 
 o=rcomp('k')
 print(len(o),o)
-#r=rdeco(o)
-#print(r)
+r=rdeco(o)
+print(r)
