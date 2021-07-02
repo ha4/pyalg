c1914515dcdee434af55f0b51679f3af007c69be
ha4 <C:\TheBat>
2021-07-03 02:11:42 +0500

    fontcomp ac3 rangecoder optimized carry error correted

--- ac3.py
+++ ac3.py
@@ -1,8 +1,6 @@
 #MIN_RANGE=radix
 #MAX_RANGE=radix*256
 SHIFT=8
-MASK=0xFFFF
-HMSK=0xFF00
 class coder_context:
     def __init__(self,vin,radix=256):
         self.r=1 # code is l/r
@@ -11,7 +9,7 @@ class coder_context:
         self.radix=radix # also min.range
         self.max=radix*radix
         self.buf=256 # for encoder
-        self.cnt=0 # carry count
+        self.cnt=0 # 0xFF carry count
         self.cnt_rr=0
         self.cnt_rw=0
         
@@ -21,57 +19,55 @@ def renorm_and_read(dc):
     dc.l*=dc.radix
     if len(dc.v)>0: dc.l+=dc.v.pop(0)
 
-def write_and_carry(dc,x,n):
+def write_and_carry(dc,x,buf):
     def put(x):
         dc.v.append(x)
     if dc.buf!=256: put(dc.buf&0xFF)
-    for _ in range(dc.cnt-1):put(x)
+    for _ in range(dc.cnt):put(x)
     dc.cnt=0
-    dc.buf=n
+    dc.buf=buf
 
 def renorm_and_write(dc):
     dc.cnt_rw+=1
     if dc.l>=dc.max:
         dc.buf+=1
-        dc.l&=MASK
+        dc.l&=dc.max-1
         if dc.cnt>0:
             dc.cnt-=1
-            write_and_carry(dc,0,256)
-            dc.buf=256
+            write_and_carry(dc,0x00,256)
     while dc.r<dc.radix:
-        if dc.l<(0xFF<<SHIFT):
-            write_and_carry(dc,0xFF,dc.l>>SHIFT)
-        else:
-            dc.cnt+=1
+        x=dc.l>>SHIFT
+        if x==0xFF: dc.cnt+=1
+        else: write_and_carry(dc,0xFF,x)
         dc.l<<=8
-        dc.l&=MASK
+        dc.l&=dc.max-1
         dc.r<<=8
 
 def encode_finish(dc):
     if dc.l>=dc.max:
         dc.buf+=1
-        dc.l&=MASK
-        c=0
+        dc.l&=dc.max-1
+        c=0x00
     else: c=0xFF
     write_and_carry(dc,c,256)
-    q=MASK
+    q=dc.max-1
     l=dc.l
     while q and l:
         dc.v.append(l>>SHIFT)
         l<<=8
-        l&=MASK
+        l&=dc.max-1
         q>>=8
 
 def renorm_freq(ct):
     if ct[0]+ct[1]>63-2:
-        ct[0]//=2
-        ct[1]//=2
+        if ct[0]>1: ct[0]//=2
+        if ct[1]>1: ct[1]//=2
 
 def dec_bit(dc,ct):
     if dc.r<dc.radix:
         renorm_and_read(dc)
-    ft=ct[0]+ct[1]+2
-    spl=dc.r*(ct[0]+1)//ft
+    ft=ct[0]+ct[1]
+    spl=dc.r*ct[0]//ft
     b=1 if dc.l>=spl else 0
     if b:
         dc.l-=spl
@@ -81,20 +77,20 @@ def dec_bit(dc,ct):
     return b
 
 def enc_bit(dc,ct,b):
-    ft=ct[0]+ct[1]+2
-    spl=dc.r*(ct[0]+1)//ft
+    renorm_and_write(dc)
+    ft=ct[0]+ct[1]
+    spl=dc.r*ct[0]//ft
     if b:
         dc.l+=spl
         dc.r-=spl
     else:
         dc.r=spl
-    renorm_and_write(dc)
 
     
-def encode(bs,sz=64):
+def encode_bits(bs,sz=64):
     dc=coder_context([])
     dc.r=dc.max
-    ct=[0,0]
+    ct=[1,1]
     bq=1<<(sz-1)
     while bq:
         b=1 if bq&bs else 0
@@ -105,9 +101,9 @@ def encode(bs,sz=64):
     encode_finish(dc)
     return dc.v
 
-def decode(v,sz=64):
+def decode_bits(v,sz=64):
     dc=coder_context(v)
-    ct=[0,0]
+    ct=[1,1]
     bq=1<<(sz-1)
     bo=0
     renorm_and_read(dc) # two radix, fill to mask
@@ -127,14 +123,16 @@ def pall(x):
     print()
 
 def test_encdec():
-    bi=0b1010101010000000000000000000000010000000000000000000000000000001
+    #bi=0b1010101010001000001000000000000010000001111111100000000000000001
+    #not adaptive:[252, 154, 25, 112] adaptive [154, 183, 2, 15, 237]
+    bi=0b0000001111111111111111111111111111111111111111111111111111111111
     sz=64
-    print("inb","{:08b}".format(bi))
-    v=encode(bi,sz)
+    print("inb","{:064b}".format(bi))
+    v=encode_bits(bi,sz)
     print("cod",end=' ')
     pall(v)
-    b=decode(v,sz)
-    print("out","{:08b}".format(b))
+    b=decode_bits(v,sz)
+    print("out","{:064b}".format(b))
 
 
 def test_diff():
