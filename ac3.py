5f508ad0ecb74acf1b9ce9fd10d615abbb806c3d
ha2 <fkufa@inbox.ru>
2021-07-02 14:55:07 +0500

    compress range-adaptive-optimized ac3

--- ac3.py
+++ ac3.py
@@ -10,8 +10,8 @@ class coder_context:
         self.v=vin
         self.radix=radix # also min.range
         self.max=radix*radix
-        self.buf=0 # carry for encoder
-        self.cnt=0
+        self.buf=256 # for encoder
+        self.cnt=0 # carry count
         self.cnt_rr=0
         self.cnt_rw=0
         
@@ -20,26 +20,27 @@ def renorm_and_read(dc):
     dc.r*=dc.radix
     dc.l*=dc.radix
     if len(dc.v)>0: dc.l+=dc.v.pop(0)
-    dc.l&=MASK
+
+def write_and_carry(dc,x,n):
+    def put(x):
+        dc.v.append(x)
+    if dc.buf!=256: put(dc.buf&0xFF)
+    for _ in range(dc.cnt-1):put(x)
+    dc.cnt=0
+    dc.buf=n
 
 def renorm_and_write(dc):
     dc.cnt_rw+=1
-    def put(x):
-        dc.v.append(x)
     if dc.l>=dc.max:
         dc.buf+=1
         dc.l&=MASK
         if dc.cnt>0:
-            put(dc.buf)
-            for _ in range(dc.cnt-1):put(0)
-            dc.buf=0
-            dc.cnt=0
+            dc.cnt-=1
+            write_and_carry(dc,0,256)
+            dc.buf=256
     while dc.r<dc.radix:
-        if dc.l<0xFF<SHIFT:
-            put(dc.buf)
-            for _ in range(dc.cnt):put(0xff)
-            dc.buf=(dc.l&HMSK)>>SHIFT
-            dc.cnt=0
+        if dc.l<(0xFF<<SHIFT):
+            write_and_carry(dc,0xFF,dc.l>>SHIFT)
         else:
             dc.cnt+=1
         dc.l<<=8
@@ -47,19 +48,18 @@ def renorm_and_write(dc):
         dc.r<<=8
 
 def encode_finish(dc):
-    def put(x):
-        dc.v.append(x)
-    c=0xFF
     if dc.l>=dc.max:
         dc.buf+=1
-        c=0x00
-    put(dc.buf)
-    for _ in range(dc.cnt): put(c)
+        dc.l&=MASK
+        c=0
+    else: c=0xFF
+    write_and_carry(dc,c,256)
     q=MASK
     l=dc.l
-    while q:
-        put((l&HMSK)>>SHIFT)
+    while q and l:
+        dc.v.append(l>>SHIFT)
         l<<=8
+        l&=MASK
         q>>=8
 
 def renorm_freq(ct):
@@ -103,8 +103,7 @@ def encode(bs,sz=64):
         renorm_freq(ct)
         bq>>=1
     encode_finish(dc)
-    print("renorms",dc.cnt_rw)
-    return
+    return dc.v
 
 def decode(v,sz=64):
     dc=coder_context(v)
@@ -119,21 +118,42 @@ def decode(v,sz=64):
         renorm_freq(ct)
         if b:bo|=bq
         bq>>=1
-    print("renorms",dc.cnt_rr)
     return bo
 
-def test_enc():
-    bi=0b00110000
-    sz=8
-    v=encode(bi,sz)
-    print(v)
+def PB(x): return "{:08b}".format(x)
+def pall(x):
+    for k in x:
+        print(PB(k),end=' ')
+    print()
 
-def test_dec():
-    sz=8
-    sq=[128+44,0,1,0,0,0]
-    b=decode(sq,sz)
-    print("{:08b}".format(b))
+def test_encdec():
+    bi=0b1010101010000000000000000000000010000000000000000000000000000001
+    sz=64
+    print("inb","{:08b}".format(bi))
+    v=encode(bi,sz)
+    print("cod",end=' ')
+    pall(v)
+    b=decode(v,sz)
+    print("out","{:08b}".format(b))
+
+
+def test_diff():
+    sx='%'
+    cx=chr_data(sx)
+    e1=cx
+    #e1=diff_v(e1)
+    #e1=diff_h(e1)
+    #e1=diff_v(e1)
+    prdata(e1)
+    cbx=data2bits(cx)
+    print("{:64b}".format(cbx))
+    v=encode(cbx,64)
+    pall(v)
+
+from ac0 import *
 
 if __name__ == "__main__":
-    test_enc()
-    #test_dec()
+    #test_binload()
+    #test_diff()
+    test_encdec()
+
